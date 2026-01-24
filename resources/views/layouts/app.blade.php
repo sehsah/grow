5ae1c2b772e0608a1b5dc6cddf9dbf9eb3063f26
sehsah <mahmoudsehsah36@gmail.com>
2026-01-25 00:16:12 +0200

    fixe menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,8 +139,10 @@
                                             <a class="flex items-center gap-2 py-2 text-sm text-muted-foreground hover:text-primary transition-colors"
                                                 href="/services/{{ $service->slug ?? '#' }}">
                                                 <div class="w-4 h-4 flex items-center justify-center">
-                                                    {!! $service->icon ??
-                                                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-wrench"><path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"></path></svg>' !!}
+                                                    @if(isset($service->icon) && !empty($service->icon))
+                                                        <img src="{{ Storage::disk('public')->url($service->icon) }}" alt="{{ $service->title }}" class="w-4 h-4 filter-primary">
+                                                    @endif
+
                                                 </div>
                                                 {{ $service->title ?? 'Service' }}
                                             </a>
